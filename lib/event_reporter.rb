d6ba59b0760791b1c77a6f30b0aa9cfdb225e0ee
Ian Andersen <ianderse@mac.com>
2014-08-11 13:45:35 -0600

    add message's for all help topics

--- lib/event_reporter.rb
+++ lib/event_reporter.rb
@@ -1,17 +1,19 @@
 require_relative 'messager'
+require_relative 'CSV_handler'
 
 class EventReporter
 
   def initialize(input, output)
     @messager = Messager.new(input, output)
     @input = input
+    @choice = []
   end
 
   def run
     @messager.welcome
     until quit?
       @messager.command
-      @choice = get_menu_input
+      @choice = get_menu_input.split(' ')
       process_user_commands
     end
     return 0
@@ -19,20 +21,62 @@ class EventReporter
 
   def process_user_commands
     case
-    when help?
-      @messager.list_commands
+    when @choice[0] == 'help' || @choice[0] == 'h'
+      help(@choice[1])
+    when @choice[0] == 'load' || @choice[0] == 'l'
+      load_command(@choice[1])
     when quit?
       @messager.quit
       return 0
     end
   end
 
-  def help(param=nil)
-    if param.downcase == "queue"
-      "explain queue"
+  def load_command(param="event_attendees.csv")
+    @handler = CSVHandler.new
+    if @handler.load_content(param) == "File does not exist."
+      @messager.file_does_not_exist
     else
-      "list commands"
+      @messager.content_loaded
     end
+  end
+
+  def help(param=nil, param2=nil)
+    case param
+    when nil
+      @messager.list_commands
+    when "queue"
+      queue
+    when "load"
+      @messager.explain_load
+    when "find"
+      @messager.explain_find
+    end
+  end
+
+  def queue
+    if @choice[3] != nil
+      param = @choice[2]+@choice[3]
+    else
+      param = @choice[2]
+    end
+
+    case param
+    when nil
+      @messager.explain_queue
+    when 'count'
+      @messager.explain_queue_count
+    when 'printby'
+      @messager.explain_queue_print_by
+    when 'print'
+      @messager.explain_queue_print
+    when 'clear'
+      @messager.explain_queue_clear
+    when 'saveto'
+      @messager.explain_queue_save_to
+    else
+      @messager.does_not_exist
+    end
+
   end
 
   def get_menu_input
@@ -40,11 +84,11 @@ class EventReporter
   end
 
   def quit?
-    @choice == 'q' || @choice == 'quit'
+    @choice.join == 'q' || @choice.join == 'quit'
   end
 
   def help?
-    @choice == 'h' || @choice == 'help'
+    @choice.join == 'h' || @choice.join == 'help'
   end
 
 end
--- lib/messager.rb
+++ lib/messager.rb
@@ -13,12 +13,21 @@ class Messager
     @output.puts "\nList of commands"
     @output.puts "help - displays this list."
     @output.puts "help <command> - description of how to use the command."
+    @output.puts "load <filename> - loads a CSV file from <filename> path, if no path given, it loads data/event_attendees.csv."
     @output.puts "queue count - output how many records are in the current queue."
     @output.puts "queue clear - empty the queue"
     @output.puts "queue print - print out a tab-delimited data table with a header row."
     @output.puts "queue print by <attribute> - print data table sorted by the specified attribute (ie. zipcode)."
     @output.puts "queue save to <filename.csv> - export current queue to specified filename as a CSV file."
     @output.puts "find <attribute> <criteria> - load the queue with all records matching criteria for the given attribute (ie. find zipcode 20011).\n\n"
+  end
+
+  def content_loaded
+    @output.puts "Content succesfully loaded."
+  end
+
+  def file_does_not_exist
+    @output.puts "File does not exist."
   end
 
   def quit
@@ -28,5 +37,46 @@ class Messager
 
   def command
     @output.print "Please enter a command (Enter 'help' for list of commands): "
+  end
+
+  def explain_queue
+    @output.puts "List of queue commands"
+    @output.puts "queue count - output how many records are in the current queue."
+    @output.puts "queue clear - empty the queue"
+    @output.puts "queue print - print out a tab-delimited data table with a header row."
+    @output.puts "queue print by <attribute> - print data table sorted by the specified attribute (ie. zipcode)."
+    @output.puts "queue save to <filename.csv> - export current queue to specified filename as a CSV file."
+  end
+
+  def explain_queue_count
+    @output.puts "queue count - output how many records are in the current queue."
+  end
+
+  def explain_queue_print_by
+    @output.puts "queue print by <attribute> - print data table sorted by the specified attribute (ie. zipcode)."
+  end
+
+  def explain_queue_print
+    @output.puts "queue print - print out a tab-delimited data table with a header row."
+  end
+
+  def explain_queue_clear
+    @output.puts "queue clear - empty the queue"
+  end
+
+  def explain_queue_save_to
+    @output.puts "queue save to <filename.csv> - export current queue to specified filename as a CSV file."
+  end
+
+  def explain_load
+    @output.puts "load <filename> - loads a CSV file from <filename> path, if no path given, it loads data/event_attendees.csv."
+  end
+
+  def explain_find
+    @output.puts "find <attribute> <criteria> - load the queue with all records matching criteria for the given attribute (ie. find zipcode 20011)."
+  end
+
+  def does_not_exist
+    @output.puts "Command does not exist, please try again."
   end
 end
