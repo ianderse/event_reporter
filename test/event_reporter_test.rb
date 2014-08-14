1b947fb801df9a0eca98da60bb600ed32b4701dc
Kavita <okavita@gmail.com>
2014-08-13 22:02:04 -0600

    Testing output more thoroughly

--- test/event_reporter_test.rb
+++ test/event_reporter_test.rb
@@ -6,11 +6,27 @@ require_relative '../lib/event_reporter'
 class EventReporterTest < Minitest::Test
 
   def setup
-    @event = EventReporter.new($stdin, $stdout)
+    @test_output = StringIO.new
+    @event = EventReporter.new($stdin, @test_output)
   end
 
   def test_it_can_call_help
+    expected_help_output = "
+List of commands
+help - displays this list.
+help <command> - description of how to use the command.
+load <filename> - loads a CSV file from <filename> path, if no path given, it loads data/event_attendees.csv.
+queue count - output how many records are in the current queue.
+queue clear - empty the queue
+queue print - print out a tab-delimited data table with a header row.
+queue print by <attribute> - print data table sorted by the specified attribute (ie. zipcode).
+queue save to <filename.csv> - export current queue to specified filename as a CSV file.
+find <attribute> <criteria> - load the queue with all records matching criteria for the given attribute (ie. find zipcode 20011).
+
+"
     @event.help
+
+  assert_equal(@test_output.string, expected_help_output)
   end
 
 end
